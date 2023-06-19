c03e2a76e56e7e28aa2d6f7ee478f68d87624604
gerhatf <gerhat@ishowroom.cz>
2023-06-19 13:25:55 +0200

    changed ios anim

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const auto = document.querySelector("#monkfu")
 
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+
+  if (event.detail.totalProgress === 0) {
+    progressBar.style.display = 'block';
+    updatingBar.style.width = '0%';
+  } else {
+    updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+
+    if (event.detail.totalProgress === 1) {
+      setTimeout(() => {
+        progressBar.style.display = 'none';
+      }, 500);
+    }
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
 function hotspotClickOne(hotspot) {
     
     document.querySelector("model-viewer").cameraOrbit = hotspot.dataset.orbit
